1f7792c6d0e3c084dc22c06a2263abb4b39dc6b1
WinterMiguelF <winterucb@gmail.com>
2019-03-08 12:29:55 -0300

    first commit

---
+++ src/Funcionario.java
@@ -0,0 +1,40 @@
+
+public abstract class Funcionario {
+
+	private String nome;
+	private String cpf;
+	private double salario;
+
+	public Funcionario() {
+
+	}
+
+	// todo funcionário recebe uma bonificação padrão, e que seu valor corresponde a
+	// 10% do salário.
+	public abstract double getBonificacao();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}
